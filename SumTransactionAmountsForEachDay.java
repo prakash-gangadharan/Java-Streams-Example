5563725bcdd8a106a5f91bb8811572cf21c72c3f
Prakash Gangadharan <prakash08.g@gmail.com>
2025-06-19 20:10:50 +0530

    Create SumTransactionAmountsForEachDay.java

---
+++ SumTransactionAmountsForEachDay.java
@@ -0,0 +1,39 @@
+import java.util.stream.*;
+import java.util.*;
+import java.util.function.*;
+
+public class SumTransactionAmountsForEachDay {
+    public static void main(String[] args) {
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction("2022-01-01", 100),
+                new Transaction("2022-01-01", 200),
+                new Transaction("2022-01-02", 300),
+                new Transaction("2022-01-02", 400),
+                new Transaction("2022-01-03", 500)
+        );
+
+        Map<String, Integer> map = transactions.stream()
+                .collect(Collectors.groupingBy(Transaction::getDate,
+                        Collectors.summingInt(Transaction::getAmount)));
+        System.out.println(map);
+        // {2022-01-03=500, 2022-01-01=300, 2022-01-02=700}
+    }
+}
+
+class Transaction {
+    String date;
+    Integer amount;
+
+    public String getDate() {
+        return date;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Transaction(String date, Integer amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+}
