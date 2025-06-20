770b036bc95dd7885a244b074431f15e33777f83
Prakash Gangadharan <prakash08.g@gmail.com>
2025-06-20 14:30:05 +0530

    Create RemoveDuplicatesWhilePreservingOrder.java

---
+++ RemoveDuplicatesWhilePreservingOrder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class RemoveDuplicatesWhilePreservingOrder {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 2, 4, 1, 5, 6, 5);
+
+        List<Integer> lst = list.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        System.out.println(lst);
+    }
+}
