9472d999a487ddc03d9bd4ac3b80c8296f4081c2
Prakash Gangadharan <prakash08.g@gmail.com>
2025-06-22 17:11:08 +0530

    Update SimpleRunnableExample.java

--- 50Java8LambdaSnippets/SimpleRunnableExample.java
+++ 50Java8LambdaSnippets/SimpleRunnableExample.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class SimpleRunnableExample {
+public class SimpleLambdaExample {
 
     public static void main(String[] args) {
         //Simple Runnable Example
@@ -12,12 +12,19 @@ public class SimpleRunnableExample {
         new Thread(r).start();
 
         //Iterating the List using Lambda
-        List<String> list1 = Arrays.asList("Java", "JavaScript", "Python");
-        list1.forEach(lst -> System.out.println(lst));
-        
+        List<String> list1 = Arrays.asList("Spring", "Java", "JavaScript", "C++","Python");
+        list1.forEach(lst -> System.out.print(lst + ", "));
+
+        //filtering list using Lambda
         List<String> list2 = list1.stream()
                 .filter(s -> s.startsWith("J"))
                 .collect(Collectors.toList());
         list2.forEach(System.out::println);
+
+        //sorting a List Using lambda
+        List<String> list3 = Arrays.asList("Spring", "Java", "JavaScript", "C++","Python");
+        list3.sort((s1,s2) -> s1.compareTo(s2));
+        list3.forEach(System.out::println);
+
     }
 }
