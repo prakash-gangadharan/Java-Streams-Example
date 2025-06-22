6ed32f181dc22db500a5f18e5fc089bd3f22fd16
Prakash Gangadharan <prakash08.g@gmail.com>
2025-06-22 16:58:20 +0530

    Update SimpleRunnableExample.java

--- 50Java8LambdaSnippets/SimpleRunnableExample.java
+++ 50Java8LambdaSnippets/SimpleRunnableExample.java
@@ -1,7 +1,23 @@
+package com.streams.lambda.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SimpleRunnableExample {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //Simple Runnable Example
         Runnable r = () -> System.out.println("Running inside from the runnable...");
         new Thread(r).start();
+
+        //Iterating the List using Lambda
+        List<String> list1 = Arrays.asList("Java", "JavaScript", "Python");
+        list1.forEach(lst -> System.out.println(lst));
+        
+        List<String> list2 = list1.stream()
+                .filter(s -> s.startsWith("J"))
+                .collect(Collectors.toList());
+        list2.forEach(System.out::println);
     }
 }
