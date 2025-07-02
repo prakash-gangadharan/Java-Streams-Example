f39e9d6cc256b43112686804e0ee4dcff51f5840
Prakash Gangadharan <prakash08.g@gmail.com>
2025-07-02 20:13:40 +0530

    Create FindMaxInArray.java

---
+++ FindMaxInArray.java
@@ -0,0 +1,18 @@
+public class FindMaxInArray {
+
+    public static int max(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 4, 7, 3};
+        System.out.println("The maximum number is : " + max(arr));
+
+    }
+}
