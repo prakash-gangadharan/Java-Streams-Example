10512fa626afa79133b6d3b4ab0568828fde11a7
Prakash Gangadharan <prakash08.g@gmail.com>
2025-07-02 20:33:27 +0530

    Update FindMaxInArray.java

--- FindMaxInArray.java
+++ FindMaxInArray.java
@@ -1,6 +1,9 @@
 public class FindMaxInArray {
 
     public static int max(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            throw new IllegalArgumentException("Array must contains at least two elements");
+        }
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
