87991d7433767f611ae18b26cd5bc4a7b8a3e979
Prakash Gangadharan <prakash08.g@gmail.com>
2025-07-02 20:32:53 +0530

    Update FindMinInArray.java

--- FindMinInArray.java
+++ FindMinInArray.java
@@ -1,5 +1,8 @@
 public class FindMinInArray {
     public static int min(int[] arr) {
+        if(arr == null || arr.length < 2){
+            throw new IllegalArgumentException("Array must contain at least two elements.");
+        }
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < min) {
